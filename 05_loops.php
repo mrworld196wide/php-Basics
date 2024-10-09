61cdc4859a363b27485d6fb011144ddd687c88a9
Aishwar Pathak <shubhum19@gmail.com>
2024-10-09 19:10:12 +0530

    loops added

---
+++ 05_loops.php
@@ -0,0 +1,21 @@
+<?php
+
+// UNCOMMENT ONE BY ONE THE BLOCKS OF CODE BELOW
+
+for ($i=0; $i < 5; $i++) {
+    echo "This is loop number $i <br>";
+}
+
+// $i=0;
+// while ($i < 5) {
+//   echo "This is loop number $i <br>";
+//   $i++;
+// }
+
+// $i = 0;
+// do {
+//     $i++;
+//     echo "This is loop number $i <br>";
+// }
+// while ($i < 5);
+?>
